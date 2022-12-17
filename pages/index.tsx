7e36314236f6614a48d7b80dbcfe50837fc68f2f
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2022-12-17 15:08:38 -0500

    You kidding me

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Let's go!
+            Let&apos;s go!
           </p>
           <div>
             <a
